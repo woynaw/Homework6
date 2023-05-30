c84a1935ab2a9936a27bd8d7311daf850858582a
Войналович Владислав <woynaw@hotmail.com>
2023-05-30 11:55:59 +0300

    add Zadacha41

--- Zadacha41/Program.cs
+++ Zadacha41/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] InputArray(int arg)
+{
+    int[] array = new int[arg];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) System.Console.Write($"{array[i]}");
+        else System.Console.Write($"{array[i]},");
+    }
+    Console.Write("]");
+}
+int Positive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine("Введите M");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = InputArray(m);
+PrintArray(array);
+Console.WriteLine($" -> {Positive(array)}");
